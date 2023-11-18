0f19ccf17bb48b571221f1318b3fe5a33561f3a9
vrajpatel1024 <vraj2.patel@ryerson.ca>
2023-11-18 15:52:17 -0500

    display working
    display working

--- script.js
+++ script.js
@@ -16,49 +16,40 @@ $(document).ready(function () {
 		});
 	}
 
-	$("#displayButton").click(function () {
-		$.ajax({
-			type: "GET",
-			url: "functions.php",
-			success: function (response) {
-				var customers = JSON.parse(response);
-				var html = "";
-				for (var i = 0; i < customers.length; i++) {
-					html +=
-						"<tr>" +
-						"<td>" +
-						customers[i].customer_id +
-						"</td>" +
-						"<td>" +
-						customers[i].first_name +
-						"</td>" +
-						"<td>" +
-						customers[i].last_name +
-						"</td>" +
-						"<td>" +
-						customers[i].email +
-						"</td>" +
-						"<td>" +
-						customers[i].phone_number +
-						"</td>" +
-						"<td>" +
-						customers[i].address +
-						"</td>" +
-						"<td>" +
-						customers[i].date_of_birth +
-						"</td>" +
-						"</tr>";
-				}
-				$("#customersTable tbody").html(html);
-			},
-		});
-	});
+    $("#displayButton").click(function () {
+        $.ajax({
+            type: "GET",
+            url: "functions.php",
+            success: function (response) {
+                var customers = JSON.parse(response);
+                var html = "<table class='table table-bordered table-striped'>";
+                html += "<thead><tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Phone Number</th><th>Address</th><th>Date of Birth</th></tr></thead><tbody>";
+    
+                for (var i = 0; i < customers.length; i++) {
+                    html +=
+                        "<tr>" +
+                        "<td>" + customers[i].customer_id + "</td>" +
+                        "<td>" + customers[i].first_name + "</td>" +
+                        "<td>" + customers[i].last_name + "</td>" +
+                        "<td>" + customers[i].email + "</td>" +
+                        "<td>" + customers[i].phone_number + "</td>" +
+                        "<td>" + customers[i].address + "</td>" +
+                        "<td>" + customers[i].date_of_birth + "</td>" +
+                        "</tr>";
+                }
+    
+                html += "</tbody></table>";
+    
+                // Display the table in the resultMessage div
+                $("#resultMessage").html(html);
+            },
+        });
+    });
     // Add an event listener for the submit button
     $("#submitButton").click(function () {
         // Assuming you have a variable 'resultString' containing the string you want to display
-        var resultString = "Submission asdasdasdsuccessful!"; // Replace with your actual result string
+        var resultString = "Subasdasdmission asdasdasdsuasdasdasdccessful!"; // Replace with your actual result string
 
-        // Display the result string in the resultMessage div
         $("#resultMessage").text(resultString);
     });
 });
